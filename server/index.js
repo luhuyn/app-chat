b6bdc068542229f25972c290c27c86ed44d93403
luhuyn <luuhuyen177@gmail.com>
2023-06-04 23:45:37 +0700

    done

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
-const express = require('express');
-const app = express();
-const http = require('http');
+var express = require('express')
+const http = require("http");
+var app = express();
 const server = http.createServer(app);
 
-const socketIo = require("socket.io")(server,{
-    cors:{
+const socketIo = require("socket.io")(server, {
+    cors: {
         origin: "*",
     }
+  });
+
+
+socketIo.on("connection", (socket) => {
+  console.log("New client connected" + socket.id);
+
+  socket.emit("getId", socket.id);
+
+  socket.on("sendDataClient", function(data) {
+    console.log(data)
+    socketIo.emit("sendDataServer", { data });
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
-socketIo.on("connection", (socket) =>{
-    console.log("New client connected" + socket.id);
-    socket.on("sendDataClient", function(data){
-        socketIo.emit("sendDataServer", { data });
-    });
-    socket.on("disconnect",()=>{
-        console.log("Client disconnected");
-    });
+app.get('/', (req, res) => {
+  // res.sendFile(__dirname + '/index.html');
 });
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on port 3000');
 });
